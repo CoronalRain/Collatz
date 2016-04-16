469412432eb24516353e2ae5de07d52fa6eb5c7b
Troy Kling <troykling1308@gmail.com>
2016-04-16 15:28:42 -0400

    Modified setup file. Upgraded to Python 3.

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name = "Collatz Sequence Toolkit",
       version = "1.0",
       description = "Mathematical tool for playing with the Collatz conjecture.",
       author = "Troy P. Kling",
       author_email = "troykling1308@gmail.com",
-      url = "https://gitlab.com/RedShift/Collatz",
+      url = "https://gitlab.com/CoronalRain/Collatz",
 	  py_modules = ["collatz"],
+	  install_requires=[
+		"csv",
+		"time",
+		"docopt"
+	  ],
      )
